515b4242586931dee8dc61ac3af9541bee696390
Ayoleke21 <olaifaayolekecm@gmail.com>
2023-11-24 14:08:28 +0100

    message

--- obj/Debug/net8.0/C#project.AssemblyInfo.cs
+++ obj/Debug/net8.0/C#project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fbc2293cbaede98b353a932e10209a576e9e08b")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
