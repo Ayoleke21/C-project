e77dd640ef1ebbe92c6e881ad75f03bb08e6e7ea
Ayoleke21 <olaifaayolekecm@gmail.com>
2023-12-07 10:46:46 +0100

    Update commit

--- obj/Debug/net8.0/C#project.AssemblyInfo.cs
+++ obj/Debug/net8.0/C#project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+735783a347a81bcec96e6a70b42cdcf4070d30c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
